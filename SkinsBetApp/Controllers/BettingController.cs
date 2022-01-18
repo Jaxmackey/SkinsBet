75145a96c42c2747d4da3a62caa62a4abae9d07a
Jax Mackey <gimaglobin@gmail.com>
2022-01-18 15:46:36 +0300

    loot

--- SkinsBetApp/Controllers/BettingController.cs
+++ SkinsBetApp/Controllers/BettingController.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Domain.Entities;
+using Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SkinsBetApp.Controllers
@@ -7,10 +11,36 @@ namespace SkinsBetApp.Controllers
     [Produces("application/json")]
     public class Betting : Controller
     {
+        private readonly ISteamRepository _steamRepository;
+
+        public Betting(ISteamRepository steamRepository)
+        {
+            this._steamRepository = steamRepository;
+        }
         [HttpPost]
         public async Task<IActionResult> TakeResult([FromBody]Skin[] skins)
         {
-            return Ok(skins);
+            var result = (await _steamRepository.GetAllSkins()).ToList();
+            if (skins.Length != result.Count)
+            {
+                return BadRequest();
+            }
+
+            for (int i = 0; i < skins.Length; i++)
+            {
+                var current = result[i];
+                if (current.Id != skins[i].Id || current.ClassId != skins[i].ClassId )
+                {
+                    return BadRequest();
+                }
+            }
+
+            var totalSkins = result.Count - 1;
+            var random = new Random();
+            Thread.Sleep(random.Next(1000, 5000));
+            var numEnd = random.Next(0, totalSkins);
+            
+            return Ok(skins[numEnd]);
         }
     }
 }
